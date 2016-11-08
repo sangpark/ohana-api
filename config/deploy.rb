1ae214135cc2255bb6be57afe3be9faf0e2b1b3d
sang park <sang.park@gmail.com>
2016-11-08 13:28:39 -0500

    Updating secrets shared link

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,10 @@ set :deploy_to, '/var/www/rails/radical_api'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/settings.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -37,10 +37,10 @@ set :deploy_to, '/var/www/rails/radical_api'
 set :ssh_options,    { :forward_agent => true, :paranoid => false }
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 set :default_env, {
