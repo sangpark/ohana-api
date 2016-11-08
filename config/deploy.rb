d728af85d25c9d38a966a8a4e3f84c0e79b8d0c4
sang park <sang.park@gmail.com>
2016-11-08 15:24:25 -0500

    Link the application yml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/var/www/rails/radical_api'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/settings.yml'
+append :linked_files, 'config/database.yml', 'config/settings.yml', 'config/application.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
