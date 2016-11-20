b9b45841f647f30fdebda3a6e5e0b76bccdd926c
spark <sang.park@gmail.com>
2016-11-20 13:08:03 -0500

    Turn digesting off

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = false
   config.assets.debug = true
 
   # `config.assets.version` and `config.assets.precompile` have moved to
@@ -57,7 +58,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   config.action_controller.perform_caching = true
 
